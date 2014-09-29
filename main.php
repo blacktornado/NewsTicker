9299040d1452e5cd754028ddb3cce36d6bc06694
pratik bhattacharya <kolkata.pratik@gmail.com>
2014-09-29 23:27:00 +0530

    Update main.php

--- main.php
+++ main.php
@@ -1,6 +1,14 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Cegura Policy</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<script type="text/javascript" src="js/jquery-1.4.min.js"></script>
+<script src="newsticker.js" type="text/javascript"></script>
+<link href="newsticker.css" rel="stylesheet" type="text/css" />
 <aside class="sidebar">
-			 <h2>Blog-News</h2>
+			 <h2>NewsTicker</h2>
 		     </aside>
 		
   <div id="wrap">
